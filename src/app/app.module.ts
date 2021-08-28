df990485a3dad5062ddbb9200da4b2104296a4df
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-28 23:46:24 +0300

    added the user, repo and detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoComponent } from './repo/repo.component';
+import { GitDetailsComponent } from './git-details/git-details.component';
+import { GitSearchComponent } from './git-search/git-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoComponent,
+    GitDetailsComponent,
+    GitSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/git-details/git-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GitDetailsComponent } from './git-details.component';
+
+describe('GitDetailsComponent', () => {
+  let component: GitDetailsComponent;
+  let fixture: ComponentFixture<GitDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GitDetailsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GitDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
