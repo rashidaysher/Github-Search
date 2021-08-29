b1480e7b94144d42bd84d272bb655ec3a419915f
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-29 21:55:15 +0300

    added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
+import { SearchUComponent } from './search-u/search-u.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'User',component:SearchComponent},
+  {path:'Repo',component:SearchUComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
