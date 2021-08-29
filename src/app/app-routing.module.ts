8b686fbd2a3393e9677bb81bcc0fd9e9b3cdb284
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-29 22:10:51 +0300

    deleted the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { SearchUComponent } from './search-u/search-u.component';
+import { GitDetailsComponent } from './git-details/git-details.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
+  {path:'',component:GitDetailsComponent},
   {path:'User',component:SearchComponent},
   {path:'Repo',component:SearchUComponent}
 ];
